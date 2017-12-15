254184c5e8b1b28c4aa212134bd35b4194b33116
Max Melentiev <melentievm@gmail.com>
2017-12-15 13:41:53 +0300

    Add project with different user & manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@ projects_count = 3
 projects_count.times { user.projects.create! } if user.projects.count < projects_count
 
 # new case
-other_user = User.find_or_create_by!(id: 2)
-project_with_same_relations = Project.create_with(user: other_user, manager: other_user).
-  find_or_create_by!(id: 10)
-project_with_same_relations = Project.create_with(user: other_user, manager: other_user).
-  find_or_create_by!(id: 12)
-project_with_different_relations = Project.create_with(user: user, manager: other_user).
-  find_or_create_by!(id: 11)
+user_2 = User.find_or_create_by!(id: 2)
+Project.create_with(user: user_2, manager: user_2).find_or_create_by!(id: 10)
+Project.create_with(user: user_2, manager: user_2).find_or_create_by!(id: 12)
+Project.create_with(user: user, manager: user_2).find_or_create_by!(id: 11)
+
+user_3 = User.find_or_create_by!(id: 3)
+Project.create_with(user: user, manager: user_3).find_or_create_by!(id: 13)
